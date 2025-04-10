f2f478defa0f26eec5ad5bc4e7d491a3744516a3
Krsatyn <92187267+krsatyn@users.noreply.github.com>
2025-04-10 14:19:30 +0400

    Update setting.py

--- setting.py
+++ setting.py
@@ -3,7 +3,7 @@
 import os
 
 
-os.environ['API_KEY'] = "vk1.a.IXJeMKChxx-fuY0-kT-bgjBygmDONvQvzhvd88fLEQWmdx025bws-iF0PeVahsSTNijyJiEP1BTqmn1OiRiKu_6k8gWkMqAHD_ejVpdIiC2cD2HHf-9jUbRPlDoU6xfSso1alz4ZJOSkB4jDbPUcq-EMNmEiql7ioHhltFfF0M6imZcnvZtc2Ex0_j4Ii-20na7U55mFW-PgpM-TtKo1NQ"
+os.environ['API_KEY'] = ""
 token = os.environ['API_KEY']
 
 link_discussion_callback = ""
